9532a768c5175d9ddbc5cfdd461fbca1c438b5ff
Ryusei Yadoumaru <ryusei0942@outlook.jp>
2022-01-13 04:55:13 +0900

    Instanceに変更

--- HewProject2022/Stage9.cpp
+++ HewProject2022/Stage9.cpp
@@ -6,54 +6,61 @@ bool GamePlay::Stage9Scene::Start()
 {
 
 	/*	オブジェクト生成	*/
-	m_Map = make_shared<Map>("stage1-1");
-	m_Player = make_shared<Player>("Player");
-	m_MainCamera = make_shared<MainCamera>("MainCamera");
-	m_Fade = make_shared<Fade>("Black");
-	m_SinkStart = make_shared<Sink>("SinkStart");
-	m_TablewareEnd = make_shared<Tableware>("TablewareEnd");
-	m_ScreenEffect = make_shared<ScreenFx>("SFX");
-	m_CameraFrame = make_shared<CameraFrame>("CFX");
+	//m_Map = make_shared<Map>("stage1-1");
+	//m_Player = make_shared<Player>("Player");
+	//m_MainCamera = make_shared<MainCamera>("MainCamera");
+	//m_Fade = make_shared<Fade>("Black");
+	//m_SinkStart = make_shared<Sink>("SinkStart");
+	//m_TablewareEnd = make_shared<Tableware>("TablewareEnd");
+	//m_ScreenEffect = make_shared<ScreenFx>("SFX");
+	//m_CameraFrame = make_shared<CameraFrame>("CFX");
 
 
-	/*	背景初期化	*/
-	m_BackGround = make_shared<BackGround>("Wall");
-	m_BackGround->Sprite("World2_BG");
-	OldInstance(m_BackGround.get());
+	///*	背景初期化	*/
+	//m_BackGround = make_shared<BackGround>("Wall");
+	//m_BackGround->Sprite("World2_BG");
+	//OldInstance(m_BackGround.get());
 
-	m_LayerBack = make_shared<LayerBack>("LayerBack");
-	m_LayerBack->Sprite("World2_obj1_4");
-	OldInstance(m_LayerBack.get());
+	//m_LayerBack = make_shared<LayerBack>("LayerBack");
+	//m_LayerBack->Sprite("World2_obj1_4");
+	//OldInstance(m_LayerBack.get());
 
-	m_LayerFront = make_shared<LayerFront>("LayerFront");
-	//m_LayerFront->Sprite("World2_obj2_1");
-	OldInstance(m_LayerFront.get());
+	//m_LayerFront = make_shared<LayerFront>("LayerFront");
+	////m_LayerFront->Sprite("World2_obj2_1");
+	//OldInstance(m_LayerFront.get());
 
-	/*	天井初期化	*/
-	m_Ceiling = make_shared<Ceiling>("Ceiling");
-	m_Ceiling->Sprite("World2_ceiling");
-	OldInstance(m_Ceiling.get());
+	///*	天井初期化	*/
+	//m_Ceiling = make_shared<Ceiling>("Ceiling");
+	//m_Ceiling->Sprite("World2_ceiling");
+	//OldInstance(m_Ceiling.get());
 
 
 	/*	インスタンス	*/
-	OldInstance(m_Map.get());
-	OldInstance(m_Player.get());
-	OldInstance(m_SinkStart.get());
-	OldInstance(m_TablewareEnd.get());
-	OldInstance(m_Fade.get());
-	OldInstance(m_ScreenEffect.get());
-	OldInstance(m_CameraFrame.get());
+	m_Map = Instance<Map>("stage1-1");
+	m_Player = Instance<Player>("Player");
+	m_MainCamera = Instance<MainCamera>("MainCamera");
+	m_SinkStart = Instance<Sink>("SinkStart");
+	m_TablewareEnd = Instance<Tableware>("TablewareEnd");
+	m_Fade = Instance<Fade>("Black");
+	m_ScreenEffect = Instance<ScreenFx>("SFX");
+	m_CameraFrame = Instance<CameraFrame>("CFX");
+	m_BackGround = Instance<BackGround>("Wall");
+	m_BackGround->Sprite("World2_BG");
+	m_LayerBack = Instance<LayerBack>("LayerBack");
+	m_LayerBack->Sprite("World2_obj1_4");
+	m_LayerFront = Instance<LayerFront>("LayerFront");
+	m_LayerFront->Sprite("World2_obj2_1");
 
 
 	/*	初期化	*/
 	m_TablewareEnd->transform->Position.x += ROAD_DISTANCE;
 
 	/*	ギミック初期化	*/
-	m_Player->m_LandTile.Init(m_Player.get(), &m_Map->m_TileColumnList);
+	m_Player->m_LandTile.Init(m_Player, &m_Map->m_TileColumnList);
 
 	/*	カメラ設定	*/
-	OldSetCamera(m_MainCamera.get());
-	m_MainCamera->Focus(m_Player.get());
+	SetCamera(m_MainCamera);
+	m_MainCamera->Focus(m_Player);
 
 	return true;
 }
@@ -112,23 +119,23 @@ bool GamePlay::Stage9Scene::Render()
 	ClearDisplay();
 
 	/****	背景	****/
-	m_BackGround->Render();
+	ObjectRender<BackGround>("Wall");
 
 	/****	後装飾品	****/
-	m_LayerBack->Render();
+	ObjectRender<LayerBack>("LayerBack");
 
 	/****	天井	****/
-	m_Ceiling->Render();
+	ObjectRender<Ceiling>("Ceiling");
 
 	/****	オブジェクト描画	****/
-	m_SinkStart->Render();
-	m_TablewareEnd->Render();
+	ObjectRender<Sink>("SinkStart");
+	ObjectRender<Tableware>("TablewareEnd");
 
-	m_Map->Render();
-	m_Player->Render();
+	ObjectRender<Map>("stage1-1");
+	ObjectRender<Player>("Player");
 
 	/****	前装飾品	****/
-	m_LayerFront->Render();
+	ObjectRender<LayerFront>("LayerFront");
 
 	/****	デバッグ	****/
 	//m_ShelfEnd->Debug();
@@ -139,8 +146,8 @@ bool GamePlay::Stage9Scene::Render()
 
 	/****	画面エフェクト	****/
 	//m_Fade->Render();
-	m_ScreenEffect->Render();
-	m_CameraFrame->Render();
+	ObjectRender<ScreenFx>("SFX");
+	ObjectRender<CameraFrame>("CFX");
 
 	/****	画面描画	****/
 	SwapChain();
